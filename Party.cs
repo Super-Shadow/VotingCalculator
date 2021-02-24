45f8346ebfe1bcfdd7296bb3ed4a318ac2544dae
Super_Shadow <Super-Shadow@users.noreply.github.com>
2021-02-24 10:01:07 +0000

    Added barebones party class and some useful vars

---
+++ Party.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VotingCalculator
+{
+	class Party
+	{
+		public string m_name;
+		public int m_iVotes;
+		public int m_iAppliedSeats;
+
+		private string m_shortName;
+		
+		public Party(string name, string shortName, int votes, int appliedSeats)
+		{
+			m_name = name;
+			m_shortName = shortName;
+			m_iVotes = votes;
+			m_iAppliedSeats = appliedSeats;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.IO;
 
 namespace VotingCalculator
 {
 	class Program
 	{
+		int iAvailableSeats = 0;
+		int iTotalVotes = 0;
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
